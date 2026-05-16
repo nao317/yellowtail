91e1bcc41df680ebbb9c686e91e356b8c686447d
nao317 <country.gentleman.0317@gmail.com>
2026-05-16 17:21:30 +0900

    #33 - 投稿調整

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -4,6 +4,7 @@ import AppErrorBoundary from '../errors/AppErrorBoundary'
 import NotFound from '../errors/NotFound'
 import HomePage from '../../pages/HomePage'
 import PostsPage from '../../pages/PostsPage'
+import PostDetailPage from '../../pages/PostDetailPage'
 
 export const router = createBrowserRouter([
   {
@@ -18,6 +19,10 @@ export const router = createBrowserRouter([
         {
           path: 'posts',
           element: <PostsPage />,
+        },
+        {
+          path: 'posts/:slug',
+          element: <PostDetailPage />,
         },
       {
         path: '*',
